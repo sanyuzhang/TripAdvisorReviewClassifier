f3e90f37725d220188565247eed366ec81712e7e
SanyuZhang <deviltrigger1120@gmail.com>
2018-12-14 18:06:34 -0500

    Changed LightGBM classifier to LightGBM regressor.

--- reviews_classifier.py
+++ reviews_classifier.py
@@ -8,24 +8,33 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 
 
-IS_TUNING = False
+IS_TUNING = True
 LINE_SEPARATOR = '\n'
 
 TARGET = 'overall_ratingsource'
-FEATURES = ['doc_id', 'city', 'country', 'num_reviews', 'sentimental', 'cleaniness', 'room', 'service', 'location', 'value', 'food']
+FEATURES = ['city', 'country', 'num_reviews']
+# FEATURES = ['doc_id', 'city', 'country', 'num_reviews', 'sentimental', 'cleaniness', 'room', 'service', 'location', 'value', 'food']
 
 
 def create_feature_sets(df):
-    # create feature sets
-    
-    df = gen_review_features(df)
+    # Create feature sets
+
+    # Remove the hotels with num_of_reviews < 0
+    df = df[df['num_reviews'] >= 0]
+
+    # Encode str type
+    for col in FEATURES:
+        df[col] = pd.Categorical(df[col])
+        df[col] = df[col].cat.codes
+
+    # df = gen_review_features(df)
 
     # for col in FEATURES:
     #     if col != TARGET:
     #         _df = df[df[col] >= 0]
     #         df[df[col] == -1] = _df[col].mean()
 
-    X = df.drop(columns=[TARGET], axis=1, inplace=False)
+    X = df[FEATURES]
     y = df[TARGET]
 
     return train_test_split(X, y, train_size=0.9, random_state=1)
@@ -123,10 +132,9 @@ def train_classifier(X_train, y_train):
         'boosting_type': 'gbdt',
         'objective': 'regression',
         'metric': 'mean_absolute_error',
-        'num_class': 5,
         'num_leaves': 15,
         "num_threads": 4,
-        'learning_rate': 0.01,
+        'learning_rate': 0.005,
         'feature_fraction': 0.4,
         'bagging_fraction': 0.6,
         'bagging_freq': 5,
@@ -145,17 +153,21 @@ def train_classifier(X_train, y_train):
 
 
 def evaluate_classifier(classifier, X_test, y_test):
-    # get the accuracy and print it
+    # Evaluate our classifier and print it
+
     y_pred = classifier.predict(X_test, num_iteration=classifier.best_iteration)
-    y_pred_max = [np.argmax(pred) for pred in y_pred]
-    # for (a, b) in zip(y_test, y_pred_max):
-    #     print(a, b)
-    print('Mean abs error is:', mean_absolute_error(y_test, y_pred_max))
+
+    # Post processing
+    # X_num_reviews = X_test['num_reviews'].values
+    # for i in range(len(X_num_reviews)):
+    #     if X_num_reviews[i] < 0: y_pred[i] = -1
+
+    print('Mean abs error is:', mean_absolute_error(y_test, y_pred))
 
 
 def train_gridcv(X_train, y_train):
     # Create classifier to use. Note that parameters have to be input manually
-    classifier = lgb.LGBMClassifier(boosting_type= 'gbdt', 
+    classifier = lgb.LGBMRegressor(boosting_type= 'gbdt', 
         objective = 'regression',
         n_jobs = 4,
         is_unbalance = True,
@@ -165,10 +177,10 @@ def train_gridcv(X_train, y_train):
 
     # Create parameters to search
     gridParams = {
-        'learning_rate': [0.005, 0.01, 0.02],
-        'n_estimators': [50, 100, 500],
-        'num_leaves': [15, 31],
-        'colsample_bytree' : [0.5, 1]
+        'learning_rate': [0.005, 0.001, 0.002],
+        'n_estimators': [100, 500, 1000],
+        'num_leaves': [7, 15, 31],
+        'colsample_bytree' : [1]
     }
 
     # Create the grid
@@ -184,7 +196,7 @@ def train_gridcv(X_train, y_train):
 
 def save_grid_results(grid):
     # Print the best parameters found
-    f = open("demo_tuning.txt", "w")
+    f = open("reviews_tuning.txt", "w")
     f.write('Best params are: ' + str(grid.best_params_) + '\n')
     f.write('Best score is: ' + str(grid.best_score_) + '\n')
     f.close()
@@ -197,17 +209,17 @@ def load_data(filename):
 
 if __name__ == '__main__':
 
-    # sample classifier on small data
+    # Sample classifier on small data
     filename = 'data/hotels.csv'
     df = load_data(filename)
 
-    # split train and test set
+    # Split train and test set
     X_train, X_test, y_train, y_test = create_feature_sets(df)
     
-    # if IS_TUNING:
-    #     train_gridcv(X_train, y_train)
-    # else:
-    #     # train classifier
-    #     classifier = train_classifier(X_train, y_train)
-    #     # evaluate
-    #     evaluate_classifier(classifier, X_test, y_test)
+    if IS_TUNING:
+        train_gridcv(X_train, y_train)
+    else:
+        # Train classifier
+        classifier = train_classifier(X_train, y_train)
+        # Evaluate
+        evaluate_classifier(classifier, X_test, y_test)
