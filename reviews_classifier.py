dbe3a2eea21698a483c46b4f8fef270cfbecec45
SanyuZhang <deviltrigger1120@gmail.com>
2018-12-14 19:52:08 -0500

    Remove outlier data.

--- reviews_classifier.py
+++ reviews_classifier.py
@@ -1,4 +1,3 @@
-import json
 import nltk
 import numpy as np
 import pandas as pd
@@ -8,12 +7,12 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 
 
-IS_TUNING = False
+IS_TUNING = True
 LINE_SEPARATOR = '\n'
 
 TARGET = 'overall_ratingsource'
-FEATURES = ['city', 'country', 'num_reviews']
-# FEATURES = ['doc_id', 'city', 'country', 'num_reviews', 'sentimental', 'cleaniness', 'room', 'service', 'location', 'value', 'food']
+# FEATURES = ['city', 'country', 'num_reviews']
+FEATURES = ['doc_id', 'city', 'country', 'num_reviews', 'sentimental', 'cleaniness', 'room', 'service', 'location', 'value', 'food']
 
 
 def create_feature_sets(df):
@@ -22,12 +21,13 @@ def create_feature_sets(df):
     # Remove the hotels with num_of_reviews < 0
     df = df[df['num_reviews'] >= 0]
 
+    # Generate features
+    df = gen_review_features(df)
+
     # Encode str type
     for col in FEATURES:
         df[col] = pd.Categorical(df[col])
         df[col] = df[col].cat.codes
-
-    # df = gen_review_features(df)
 
     # for col in FEATURES:
     #     if col != TARGET:
@@ -59,8 +59,8 @@ def gen_review_features(df):
         filename = fileroot + doc
 
         openfile = open(filename,  'r', encoding='utf8', errors='ignore')
-        raw = openfile.read() #.decode('utf8', 'ignore')
-        reviews = raw.split(LINE_SEPARATOR)
+        reviews = openfile.read().split(LINE_SEPARATOR)
+        openfile.close()
 
         # Add or fix feature extraction functions below
         df.loc[df['doc_id'] == doc, 'sentimental'] = sentimental_from_review(reviews)
@@ -132,13 +132,13 @@ def train_classifier(X_train, y_train):
         'boosting_type': 'gbdt',
         'objective': 'regression',
         'metric': 'mean_absolute_error',
-        'num_leaves': 15,
+        'num_leaves': 7,
         "num_threads": 4,
         'learning_rate': 0.005,
         'feature_fraction': 0.4,
         'bagging_fraction': 0.6,
         'bagging_freq': 5,
-        'n_estimators': 500,
+        'n_estimators': 1000,
         'verbose': 0
     }
 
@@ -162,7 +162,7 @@ def evaluate_classifier(classifier, X_test, y_test):
     # for i in range(len(X_num_reviews)):
     #     if X_num_reviews[i] < 0: y_pred[i] = -1
 
-    print('Mean abs error is:', mean_absolute_error(y_test, y_pred))
+    print('Mean absolute error is:', mean_absolute_error(y_test, y_pred))
 
 
 def train_gridcv(X_train, y_train):
@@ -177,9 +177,9 @@ def train_gridcv(X_train, y_train):
 
     # Create parameters to search
     gridParams = {
-        'learning_rate': [0.005, 0.001, 0.002],
-        'n_estimators': [100, 500, 1000],
-        'num_leaves': [7, 15, 31],
+        'learning_rate': [0.005],
+        'n_estimators': [1000],
+        'num_leaves': [3, 7, 15],
         'colsample_bytree' : [1]
     }
 
@@ -215,7 +215,7 @@ if __name__ == '__main__':
 
     # Split train and test set
     X_train, X_test, y_train, y_test = create_feature_sets(df)
-    
+
     if IS_TUNING:
         train_gridcv(X_train, y_train)
     else:
